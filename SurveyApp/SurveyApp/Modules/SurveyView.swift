1b23510a956c2fac79086f9c9afc7b3c33bb3309
Oleg Kasarin <>
2023-11-15 18:15:48 +0100

    Set up project structure

---
+++ SurveyApp/SurveyApp/Modules/SurveyView.swift
@@ -0,0 +1,18 @@
+//
+//  SurveyView.swift
+//  SurveyApp
+//
+//  Created by Oleg Kasarin on 15/11/2023.
+//
+
+import SwiftUI
+
+struct SurveyView: View {
+    var body: some View {
+        Text("2")
+    }
+}
+
+#Preview {
+    SurveyView()
+}
