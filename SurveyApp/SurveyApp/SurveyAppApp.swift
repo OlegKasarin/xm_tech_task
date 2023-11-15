9d9a67ebdbe79132bf0c8bf2f10e2820371bec20
Oleg Kasarin <>
2023-11-15 18:09:43 +0100

    Set up project

---
+++ SurveyApp/SurveyApp/SurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyAppApp.swift
+//  SurveyApp
+//
+//  Created by Oleg Kasarin on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
